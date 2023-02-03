97aef2dc1d03afdd6e80fad25885ca4e4eda8828
github-jihyolic <ji2hyo5@gmail.com>
2023-02-03 19:01:52 +0900

    swiper 추가

---
+++ script/swiper.js
@@ -0,0 +1,12 @@
+$(function () {
+  var swiper = new Swiper(".mySwiper", {
+    pagination: {
+      el: ".swiper-pagination",
+      type: "progressbar",
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+});
